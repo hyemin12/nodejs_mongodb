a8f22aabc89430e59d1688225c99b2d5518d7293
hyemin12 <khnm31@naver.com>
2022-10-17 18:28:38 +0900

    'store생성'

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import Reduxthunk from "redux-thunk";
+import rootReducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  Reduxthunk
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider
+      store={createStoreWithMiddleware(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
